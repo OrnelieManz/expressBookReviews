094748fd7dd0b40ba1cb2ac4608a6b70417562ff
OrnelieManz <ornelie.manz@gmail.com>
2024-07-03 10:17:07 -0400

    updating authentification codes

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -3,18 +3,39 @@ let books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
+
 const deepObjFilter = (pred) => (o) => Object .fromEntries (
     pathEntries (o) .filter (([p, v]) => pred (v)) .map (([p, v]) => [p .at (-1), v])
   );
+
 const pathEntries = (o, p = []) => [
     ... (p .length > 0 ? [[p, o]] : []),
     ... (Object (o) === o ? Object .entries (o) .flatMap (
           ([k, v]) => pathEntries (v, [...p, Array .isArray (o) ? Number (k) : k])) : []
         )
   ];
+
+  
 public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+
+    const username = req.body.username;
+    const password = req.body.password;
+
+    // Check if both username and password are provided
+    if (username && password) {
+        // Check if the user does not already exist
+        if (isValid(username)) {
+            // Add the new user to the users array
+            users.push({"username": username, "password": password});
+            return res.status(200).json({message: "User successfully registered. Now you can login"});
+        } else {
+            return res.status(404).json({message: "User already exists!"});
+        }
+    }
+    // Return error if username or password is missing
+    return res.status(404).json({message: "Unable to register user."});
+  
+  //return res.status(300).json({message: "Yet to be implemented"});
 });
 
 // Get the book list available in the shop
